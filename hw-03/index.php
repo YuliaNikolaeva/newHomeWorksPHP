37b36f8b5dc5f7917686c4242a3376a74e60c230
Yulia Nikolaeva <nikolyulia@gmail.com>
2021-05-16 07:35:41 +0300

    hw-03, part 2, fin

--- hw-03/index.php
+++ hw-03/index.php
@@ -3,4 +3,7 @@
     $client = new \GuzzleHttp\Client();
     $response = $client->request('GET', 'https://itea.ua');
     echo $response->getBody();
-?>
+
+    $bigText = new \caps\Caps();
+    echo $bigText-> makeCaps('Text for example') . PHP_EOL;
+?>clear
